7fc994d8602f1522fa919d6713aca16ab686d48c
David Roegiers <david.roegiers@gmail.com>
2018-09-14 10:46:40 +0200

    fix

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.http import (HttpResponseRedirect,
                          Http404,
+                         HttpResponse,
                          HttpResponsePermanentRedirect)
 
 from .misc import (hash_encode,
